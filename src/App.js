8c064d0f48f7e85d1c0c8898525d97fc3729b21d
Ross Duris <rossduris@gmail.com>
2023-01-03 16:04:24 -0500

    added auto complete search suggestions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
 
 const [searchResults, setSearchResults] = useState(JSON)
-
 const [searchValue, setSearchValue] = useState('')
+const autoListRef = useRef(undefined)
+let searchSuggestion = autoListRef.current
 
 const handleSubmit = (e) => {
   e.preventDefault();
@@ -27,62 +28,117 @@ const handleSubmit = (e) => {
     }
   };
 
-  axios.request(options).then(function (response) {
-    console.log('api request made');
-    setSearchResults(response.data)
-  }).catch(function (error) {
-    console.log('api had an error');
-    console.error(error);
-  });
-}
+    axios.request(options).then(function (response) {
+      console.log('api request made');
+      setSearchResults(response.data)
+    }).catch(function (error) {
+      console.log('api had an error');
+      console.error(error);
+    });
+  }
 
-const domainList =  [
-  "google.com",
-  "abc.com",
-  "google.uk",
-  "google.us",
-  'speedtrain.coffee',
-  'guavasoup.com',
-  'riftphase.ai',
-  'fusionmonkey.co',
-  'perfectsetup.trade',
-  'prailinetown.com',
-  'perfectstuff.org',
-  'hariyplanet.com'
-]
+  const domainList =  [
+    "google.com",
+    "abc.com",
+    "google.uk",
+    "google.us",
+    'speedtrain.coffee',
+    'guavasoup.com',
+    'riftphase.ai',
+    'fusionmonkey.co',
+    'perfectsetup.trade',
+    'prailinetown.com',
+    'perfectstuff.org',
+    'hariyplanet.com'
+  ]
 
-const [whoisData, setWhoisData] = useState(JSON)
+  const [whoisData, setWhoisData] = useState(JSON)
+  const [autoCompleteWords, setAutoCompleteWords] = useState(JSON)
 
 
 
+  useEffect(() => {    
+    if(searchValue.length > 0){
+      getDatamuseWords()
+      if(searchSuggestion){
+        searchSuggestion.style.display = "block"
+      }
+      
+    }
+    if(searchValue.length < 1){
+      if(searchSuggestion){
+        searchSuggestion.style.display = "none"
+      }
+     
+    }
+    
+  }, [searchValue])
+
+
+  const getDatamuseWords = async () => {    
+    const options = {
+      method: 'GET',
+    };
+  
+    try{
+      const response = await fetch(`https://api.datamuse.com/words?sp=${searchValue}??`, options)
+      const data = await response.json()
+      setAutoCompleteWords(data)
+    } catch (err) {
+      console.log("There was an error" + err)
+    }
+  }
+  
+  const handleBlur = () => {
+    
+    searchSuggestion.style.display = "none"
+    
+  }
+ 
+  const handleFocus = () => {
+    if(searchValue.length > 1){
+      searchSuggestion.style.display = "block"
+    }
+    
+    console.log('hide list')
+  }
+
+  
+
+
+  // getDatamuseWords()
+
+  const wordList = Object.entries(autoCompleteWords)
   const entries = Object.entries(searchResults)
-  const whoEntries = Object.entries(whoisData)
 
-  whoEntries.map((entry) => {
-    console.log(entry)
-  })
 
   return (
     <div className="App">
-      <header className="App-header">
+      
+
+       <div className='mainWrapper'>
        <h1>Domain Search</h1> 
-       <form>
-        <input className='searchBox' type="text" onChange={(e) => setSearchValue(e.target.value)} placeholder='Search domains' />
-        <button type='submit' onClick={handleSubmit}>Search</button>
-       </form>
-       <div>
-        {entries.map((entry) => {
-          const {domain, status} = entry[1][0]
-          return(<div key={domain}>{domain} {status}</div>)
-        })}
+        <form className='searchForm'>
+          <input className='searchBox' type="text" onFocus={handleFocus} onBlur={handleBlur} 
+            onChange={(e) => {
+            setSearchValue(e.target.value)}
+            } 
+            placeholder='Search domains' />
+          <button type='submit' onClick={handleSubmit}>Search</button>
+        </form>
+        <div className='autolist' ref={autoListRef}>
+          {wordList.map((w) => {      
+            return(<li>{JSON.stringify(w[1].word)}</li>)
+          })}
+        </div>      
+        <div className='results'>
+          {entries.map((entry) => {
+            const {domain, status} = entry[1][0]          
+            return(<div key={domain}>{domain} {status.includes('undelegated')?<span style={{color:'green'}}>Available</span>:<span style={{color:'red'}}>Not Available</span>}</div>)
+          })}
+        </div>
        </div>
-       <div>
-        {whoEntries.map((entry) => {
-          const {results} = entry[1]
-          return(<div>{entry[1].length}</div>)
-        })}
-       </div>
-      </header>
+      
     </div>
   );
 }
